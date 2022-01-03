aa5949477cf824d7f4a5c58c1cd1004cde8b2fd3
unknown <parinaaz.cobla@gmail.com>
2022-01-03 13:14:55 +0530

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+//const { urlencoded } = require('body-parser');
+const app = express();
+app.listen(3000,function(){
+    console.log("Hello from Arzaan baby");
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/article",(req,res)=>{
+    res.send("Hi this is article");
+    console.log(req.body)
+    
+})
